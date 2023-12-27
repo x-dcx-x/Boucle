d9995e7ffd126f04bd5e0021027c380663009e69
x-dcx-x <112556623+x-dcx-x@users.noreply.github.com>
2023-12-27 17:47:10 +0100

    Initial commit

---
+++ public/assets/js/app.js
@@ -0,0 +1,9 @@
+
+    // Boucle de 1 à 10
+    for (let i = 1; i <= 10; i++) {
+    // Vérifier si le nombre est pair ou impair
+    let message = (i % 2 === 0) ? "Pair" : "Impair";
+
+    // Afficher le message dans la console
+    console.log("Le nombre " + i + " est " + message);
+}
